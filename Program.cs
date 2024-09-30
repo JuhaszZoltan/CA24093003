2d2a136db28ef8724f55298fc9131e5515d1eb8e
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-09-30 15:03:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using CA24093003;
+using System.Text;
+
+Random rnd = new();
+List<Recept> szakacskonyv = [];
+
+using StreamReader sr = new(@"..\..\..\src\szakacskonyvCLI_adatok.csv",Encoding.UTF8);
+while (!sr.EndOfStream) szakacskonyv.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"3. feladat: az allomanyban {szakacskonyv.Count} recept szerepel.");
+
+var f4 = szakacskonyv.Count(r => r.Ido < 65);
+Console.WriteLine($"4. feladat: {f4} recept keszitheto el 65 percnel rovidebb ido alatt");
+
+var f5 = szakacskonyv.GroupBy(r => r.Fajta);
+Console.WriteLine("5. feladat: etelfajtak - darab");
+foreach (var fajat in f5)
+{
+    Console.WriteLine($"\t{fajat.Key}: {fajat.Count()} db");
+}
+
+var f6 = szakacskonyv.Where(r => r.Nehezseg == "könnyű").ToList();
+Console.WriteLine($"6. feladat: pl. egy konnyu etel: {f6[rnd.Next(f6.Count)]}");
---
+++ Recept.cs
@@ -0,0 +1,24 @@
+﻿namespace CA24093003
+{
+    internal class Recept
+    {
+        public string Etel { get; set; }
+        public int Ido { get; set; }
+        public string Fajta { get; set; }
+        public string Nehezseg { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Etel} ({Nehezseg}, {Ido} perc, {Fajta})";
+        }
+
+        public Recept(string sor)
+        {
+            var v = sor.Split(';');
+            Etel = v[0];
+            Ido = int.Parse(v[1]);
+            Fajta = v[2];
+            Nehezseg = v[3];
+        }
+    }
+}
